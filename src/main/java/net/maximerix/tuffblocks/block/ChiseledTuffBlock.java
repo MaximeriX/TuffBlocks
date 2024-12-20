cbd12552bb1570fbeee8fd1e93cdaec0b173cdde
MaximeriX <savickijmaksim137@gmail.com>
2024-12-20 21:51:31 +0200

    Forge 1.18.2 - 1.0.8 changes

--- src/main/java/net/maximerix/tuffblocks/block/ChiseledTuffBlock.java
+++ src/main/java/net/maximerix/tuffblocks/block/ChiseledTuffBlock.java
@@ -1,25 +1,18 @@
 package net.maximerix.tuffblocks.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.common.util.ForgeSoundType;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class ChiseledTuffBlock extends Block {
 	public ChiseledTuffBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.TERRACOTTA_GRAY)
-				.sound(new ForgeSoundType(1.0f, 1.0f,
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.break")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.step")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.place")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.hit")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.fall"))))
+				.sound(SoundType.TUFF)
 				.strength(1.5f, 6f).requiresCorrectToolForDrops());
 	}
 
--- src/main/java/net/maximerix/tuffblocks/block/TuffSlabBlock.java
+++ src/main/java/net/maximerix/tuffblocks/block/TuffSlabBlock.java
@@ -1,22 +1,15 @@
 package net.maximerix.tuffblocks.block;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.common.util.ForgeSoundType;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class TuffSlabBlock extends SlabBlock {
 	public TuffSlabBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.TERRACOTTA_GRAY)
-				.sound(new ForgeSoundType(1.0f, 1.0f,
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.break")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.step")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.place")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.hit")),
-						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("tuff:block.tuff.fall"))))
+				.sound(SoundType.TUFF)
 				.strength(1.5f, 6f).requiresCorrectToolForDrops().dynamicShape());
 	}
 }
